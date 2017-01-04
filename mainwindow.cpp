12b3135db6c2da66a3f8101d953c4ded65d08eac
mititeluteofil <mititelu.teofil@yahoo.ro>
2017-01-04 19:09:13 +0200

    Menu updated Precision problem solved

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <iomanip>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -29,4 +30,14 @@ void MainWindow::on_comboBox_activated(const QString &arg1)
 void MainWindow::on_comboBox_editTextChanged(const QString &arg1)
 {
 
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    double input , output;
+    input=ui->lineEdit_1->text().toDouble();
+    output=input*39.37007874015748;
+     std::setprecision(12);
+    double value = 312345678952.1415926535897931;
+    ui->lineEdit_1a->setText(QString::number( value, 'f', 10 ));
 }
